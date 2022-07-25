e5fbd612f2f0c9836550eedd10b3ab1d4500bad2
adrianlara24 <adrianlara24@gmail.com>
2022-07-25 18:54:57 -0300

    docs 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  
+  <div class="btn">
+    <input type="file" (change)="upload($event)">
+  </div>
+  <div class="images">
+    <img *ngFor="let item of images"  [src]="item" />
+  </div>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  images: any[] = [];
+
+  upload(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.onload = e => this.images.push(reader.result);
+
+      reader.readAsDataURL(file);
+    }
+
+  }
 }
